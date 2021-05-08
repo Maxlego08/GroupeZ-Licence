34607339ffbaa8464cca67ca67a0a3f5e4d9c1c9
Maxlego08 <maxlego08@sfr.fr>
2021-05-08 11:17:52 +0200

    :construction: Add licence from argument

--- src/fr/groupez/licence/Licence.java
+++ src/fr/groupez/licence/Licence.java
@@ -5,7 +5,13 @@ import java.net.UnknownHostException;
 public class Licence {
 
 	public static void main(String[] args) throws UnknownHostException {
-		String licence = "iw7yXu45fJeAIwa";
+
+		if (args.length != 1) {
+			System.out.println("Invalid argument: java -jar Licence.jar <licence>");
+			return;
+		}
+
+		String licence = args[0];
 		LicenceVerif licenceVerif = new LicenceVerif(licence);
 		licenceVerif.setEnableLog(true);
 		licenceVerif.asynCheck(() -> {
